3a90fedafabf557faff4e10375466a309f6dc040
ryan <ryan@vectorblox.com>
2016-08-03 09:20:52 -0700

    Updated software interrupt test for use in hardware.

--- software/interrupt_test.c
+++ software/interrupt_test.c
@@ -7,6 +7,12 @@ volatile uint32_t software_flag = 0;
 volatile uint32_t external_flag = 0; 
 
 #define SYS_CLK 125e5
+#define HARDWARE_TEST 1
+#define LEDR ((volatile int *) (0x01000010))
+
+#if HARDWARE_TEST
+volatile uint32_t interrupt_mask = 0x00000000;
+#endif
 
 int main(void) {
 
@@ -25,9 +31,15 @@ int main(void) {
 #define EXT_INT_TO_TEST 14
   // Interrupts failed...
   if (!(software_flag & timer_flag) || (external_flag != EXT_INT_TO_TEST)) {
+#if HARDWARE_TEST
+    *LEDR = 0xFFFFFFFF;
+#endif 
     TEST_FAIL();
   }
 
+#if HARDWARE_TEST
+    *LEDR = interrupt_mask;
+#endif 
   TEST_PASS();
   return 1;
 }
@@ -41,12 +53,18 @@ long handle_interrupt(long cause, long epc, long regs[32])
       // Clear pending software interrupt.
       clear_software_interrupt();
       software_flag = 1;
+#if HARDWARE_TEST
+      interrupt_mask |= 0x1;
+#endif
       break;
 
     case M_TIMER_INTERRUPT:
       // Safe sequence of disabling timer interrupt without accidental interrupts.
       clear_timer_interrupt_cycles();
       timer_flag = 1;
+#if HARDWARE_TEST
+      interrupt_mask |= 0x2;
+#endif
       break;
 
     case M_EXTERNAL_INTERRUPT:
@@ -127,6 +145,9 @@ long handle_interrupt(long cause, long epc, long regs[32])
       */
 
       external_flag = plic_claim;
+#if HARDWARE_TEST
+      interrupt_mask |= 0x4;
+#endif
       
       // Write to INTRPT_COMPLETE to signify that the PLIC can 
       // receive another external interrupt from the source it
