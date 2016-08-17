989873dab43c0cc54c89721643005e97e5b71fb9
Joel Vandergriendt <joel@vectorblox.com>
2016-08-17 08:12:01 -0700

    generate_hex_files now builds from git submodule of riscv-tools

--- tools/generate_hex_files.sh
+++ tools/generate_hex_files.sh
@@ -1,10 +1,23 @@
 #!/bin/sh
-TEST_DIR=~ryan/riscv-tools/riscv-tests/isa
+SCRIPTDIR=$(dirname $0)
+
+echo "initializing git submodules containing tests, and building them"
+git submodule update --init $SCRIPTDIR/riscv-toolchain/riscv-tools/
+(cd $SCRIPTDIR/riscv-toolchain/riscv-tools/riscv-tests/ && git submodule update --init --recursive . )
+
+TEST_DIR=$SCRIPTDIR/riscv-toolchain/riscv-tools/riscv-tests/isa
+
+(cd $TEST_DIR/../ && ./configure --with-xlen=32 && make isa ) 2>/dev/null 1>&2
+
+
 SOFTWARE_DIR=../software
 #all files that aren't dump or hex (the hex files are not correctly formatted)
 FILES=$(ls ${TEST_DIR}/rv32ui-p-* | grep -v dump | grep -v hex)
 ORCA_FILES=$(ls ${SOFTWARE_DIR}/* | grep .elf)
 
+PREFIX=riscv32-unknown-elf
+OBJDUMP=$PREFIX-objdump
+OBJCOPY=$PREFIX-objcopy
 
 if which mif2hex >/dev/null
 then
@@ -17,18 +30,19 @@ mkdir -p test
 
 
 #MEM files are for lattice boards, the hex files are for altera boards
-for f in $FILES
+for f in $FILES $ORCA_FILES
 do
-	 echo "$f > test/$(basename $f).qex"
+
+	 BIN_FILE=test/$(basename $f).bin
+	 QEX_FILE=test/$(basename $f).qex
+	 MEM_FILE=test/$(basename $f).mem
+	 MIF_FILE=test/$(basename $f).mif
+	 SPLIT_FILE=test/$(basename $f).split2
+	 echo "$f > $QEX_FILE"
 	 (
-		  BIN_FILE=test/$(basename $f).bin
-		  QEX_FILE=test/$(basename $f).qex
-		  MEM_FILE=test/$(basename $f).mem
-		  MIF_FILE=test/$(basename $f).mif
-		  SPLIT_FILE=test/$(basename $f).split2
 		  cp $f test/
-		  riscv64-unknown-elf-objcopy -O binary $f $BIN_FILE
-		  riscv64-unknown-elf-objdump -D $f > test/$(basename $f).dump
+		  $OBJCOPY -O binary $f $BIN_FILE
+		  $OBJDUMP -D $f > test/$(basename $f).dump
 
 		  python ../tools/bin2mif.py $BIN_FILE 0x200 > $MIF_FILE || exit -1
 		  mif2hex $MIF_FILE $QEX_FILE >/dev/null 2>&1 || exit -1
@@ -36,27 +50,5 @@ do
 		  awk '{if (NF == 9) print $5$6$7$8}' $SPLIT_FILE > $MEM_FILE
 		 # rm -f $MIF_FILE $SPLIT_FILE
 	 ) &
-done
-
-for f in $ORCA_FILES
-do
-  FILE=$(basename "$f")
-  FILE="${FILE%.*}"
-  echo "$f > test/$FILE.qex"
-  (
-    BIN_FILE=test/$(basename $FILE).bin
-    QEX_FILE=test/$(basename $FILE).qex
-    MEM_FILE=test/$(basename $FILE).mem
-    MIF_FILE=test/$(basename $FILE).mif
-    SPLIT_FILE=test/$(basename $FILE).split2
-    cp $f test/
-    riscv64-unknown-elf-objcopy -O binary $f $BIN_FILE
-    riscv64-unknown-elf-objdump -D $f > test/$FILE.dump
-
-    python ../tools/bin2mif.py $BIN_FILE 0x100 > $MIF_FILE || exit -1
-    mif2hex $MIF_FILE $QEX_FILE >/dev/null 2>&1 || exit -1
-    sed -e 's/://' -e 's/\(..\)/\1 /g'  $QEX_FILE >$SPLIT_FILE
-    awk '{if (NF == 9) print $5$6$7$8}' $SPLIT_FILE > $MEM_FILE
-  ) &
 done
 wait
