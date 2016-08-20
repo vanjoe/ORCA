30ba72d44cff466487df1d45cef3190925dfe499
ryan <ryan@vectorblox.com>
2016-08-19 18:05:11 -0700

    Acc pointers working.

--- software/beamforming.c
+++ software/beamforming.c
@@ -21,18 +21,13 @@ extern int samples_l[NUM_SAMPLES];
 extern int samples_r[NUM_SAMPLES];
 
 //extern int fir_taps[NUM_TAPS];
-
+#define cause_write(a) asm volatile ("csrw mscratch, %0"	\
+				     :				\
+				     : "r" (a))
 int main() {
   
   //TODO Use mics properly
   int sample_count = 0;
-
-  int32_t i;
-  int32_t buffer_count = 0;
-  int32_t power_front = 0;
-  int32_t power_left = 0;
-  int32_t power_right = 0;
-  int32_t transfer_offset;
 
   vbx_word_t *mic_buffer_l = SCRATCHPAD_BASE;
   vbx_word_t *mic_buffer_r = SCRATCHPAD_BASE + BUFFER_LENGTH;
@@ -40,12 +35,30 @@ int main() {
   vbx_word_t *sound_vector_r = sound_vector_l + WINDOW_LENGTH + SAMPLE_DIFFERENCE;
   vbx_word_t *sum_vector = sound_vector_r + WINDOW_LENGTH + SAMPLE_DIFFERENCE;
 
+  int32_t i;
+  int32_t buffer_count = 0;
+  int32_t *power_front = sum_vector + WINDOW_LENGTH;
+  int32_t *power_left = power_front + 1;
+  int32_t *power_right = power_left + 1;
+  int32_t transfer_offset;
+  cause_write(power_front);
+  cause_write(power_left );
+  cause_write(power_right);
+
   vbx_set_vl(BUFFER_LENGTH);
   vbx(SEWS, VAND, mic_buffer_l, 0, vbx_ENUM);
   vbx(SEWS, VAND, mic_buffer_r, 0, vbx_ENUM);
   vbx_set_vl(WINDOW_LENGTH + SAMPLE_DIFFERENCE);
   vbx(SEWS, VAND, sound_vector_l, 0, vbx_ENUM);
   vbx(SEWS, VAND, sound_vector_r, 0, vbx_ENUM);
+
+  cause_write(mic_buffer_l[0]);
+  for(i = 0; i < BUFFER_LENGTH; i++) {
+    cause_write(i);
+    cause_write(mic_buffer_l[i]);
+    cause_write(mic_buffer_r[i]);
+  }
+
 
   while (1) {
 
@@ -80,36 +93,35 @@ int main() {
     vbx(VVWS, VMOV, (sound_vector_l + SAMPLE_DIFFERENCE), (mic_buffer_l + transfer_offset), 0);
     vbx(VVWS, VMOV, (sound_vector_r + SAMPLE_DIFFERENCE), (mic_buffer_r + transfer_offset), 0);
 
-    power_front = 0;
-    power_left = 0;
-    power_right = 0;
-
     // Calculate the power assuming the sound is coming from the front.
     vbx(VVWS, VADD, sum_vector, (sound_vector_l + SAMPLE_DIFFERENCE), (sound_vector_r + SAMPLE_DIFFERENCE));
-    vbx_acc(VVWS, VMUL, &power_front, sum_vector, sum_vector);
+    vbx_acc(VVWS, VMUL, power_front, sum_vector, sum_vector);
+    for(i = 0; i < WINDOW_LENGTH; i++) {
+      cause_write(i);
+      cause_write(sum_vector[i]); 
+    }
 
     // Calculate the power assuming the sound is coming from the left (right microphone is 
     // delayed during sampling, so delay left microphone to compensate).
     vbx(VVWS, VADD, sum_vector, sound_vector_l, (sound_vector_r + SAMPLE_DIFFERENCE));
-    vbx_acc(VVWS, VMUL, &power_left, sum_vector, sum_vector);
+    vbx_acc(VVWS, VMUL, power_left, sum_vector, sum_vector);
+    for(i = 0; i < WINDOW_LENGTH; i++) {
+      cause_write(i);
+      cause_write(sum_vector[i]); 
+    }
     
     // Calculate the power assuming the sound is coming from the right (left microphone is delayed
     // during sampling, so delay right microphone to compensate).
     vbx(VVWS, VADD, sum_vector, (sound_vector_l + SAMPLE_DIFFERENCE), sound_vector_r);
-    vbx_acc(VVWS, VMUL, &power_right, sum_vector, sum_vector); 
-    for (i = 0; i < WINDOW_LENGTH; i++) {
-      power_right += sum_vector[i] * sum_vector[i];
-      asm volatile("csrw mscratch, %0\n"
-                   "csrw mscratch, %1\n"
-                   "csrw mscratch, %2\n"
-                   "csrw mscratch, %3"
-        :
-        : "r" (i), "r" (sum_vector[i]), "r" (sound_vector_l[i + SAMPLE_DIFFERENCE]), "r" (sound_vector_l[i]));
+    vbx_acc(VVWS, VMUL, power_right, sum_vector, sum_vector); 
+    for(i = 0; i < WINDOW_LENGTH; i++) {
+      cause_write(i);
+      cause_write(sum_vector[i]); 
     }
 
     asm volatile("csrw mscratch, %0\n csrw mscratch, %1\n csrw mscratch, %2"
       :
-      : "r" (power_front), "r" (power_left), "r" (power_right));
+      : "r" (*power_front), "r" (*power_left), "r" (*power_right));
     
 
     if (power_front > power_left) {
