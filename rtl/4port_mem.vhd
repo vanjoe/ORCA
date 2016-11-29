1f28e4874abc23ca36b8b78922d7adf372c1d8f9
Joel Vandergriendt <joel@vectorblox.com>
2016-11-28 16:24:33 -0800

    dual port memory works with quartus and vsim

--- rtl/4port_mem.vhd
+++ rtl/4port_mem.vhd
@@ -1,5 +1,70 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.all;
+
+library work;
+use work.utils.all;
+
+entity true_dual_port_ram_single_clock is
+
+  generic
+    (
+      DATA_WIDTH : natural := 8;
+      ADDR_WIDTH : natural := 6
+      );
+
+  port
+    (
+      clk    : in  std_logic;
+      addr_a : in  natural range 0 to 2**ADDR_WIDTH - 1;
+      addr_b : in  natural range 0 to 2**ADDR_WIDTH - 1;
+      data_a : in  std_logic_vector((DATA_WIDTH-1) downto 0);
+      data_b : in  std_logic_vector((DATA_WIDTH-1) downto 0);
+      we_a   : in  std_logic := '1';
+      we_b   : in  std_logic := '1';
+      q_a    : out std_logic_vector((DATA_WIDTH -1) downto 0);
+      q_b    : out std_logic_vector((DATA_WIDTH -1) downto 0)
+      );
+
+end true_dual_port_ram_single_clock;
+
+architecture rtl of true_dual_port_ram_single_clock is
+
+  -- Build a 2-D array type for the RAM
+  subtype word_t is std_logic_vector((DATA_WIDTH-1) downto 0);
+  type memory_t is array(2**ADDR_WIDTH-1 downto 0) of word_t;
+
+  -- Declare the RAM
+  shared variable ram : memory_t;
+
+begin
+
+
+  -- Port A
+  process(clk)
+  begin
+    if(rising_edge(clk)) then
+      if(we_a = '1') then
+        ram(addr_a) := data_a;
+      end if;
+      q_a <= ram(addr_a);
+    end if;
+  end process;
+
+  -- Port B
+  process(clk)
+  begin
+    if(rising_edge(clk)) then
+      if(we_b = '1') then
+        ram(addr_b) := data_b;
+      end if;
+      q_b <= ram(addr_b);
+    end if;
+  end process;
+
+end rtl;
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
 use IEEE.numeric_std.all;
 use IEEE.STD_LOGIC_TEXTIO.all;
 use STD.TEXTIO.all;
@@ -33,39 +98,73 @@ end entity ram_2port;
 
 
 architecture behav of ram_2port is
+  component true_dual_port_ram_single_clock is
 
-  type ram_type is array(0 to MEM_DEPTH-1) of std_logic_vector(MEM_WIDTH -1 downto 0);
+    generic
+      (
+        DATA_WIDTH : natural := 8;
+        ADDR_WIDTH : natural := 6
+        );
 
-  shared variable ram : ram_type;
+    port
+      (
+        clk    : in  std_logic;
+        addr_a : in  natural range 0 to 2**ADDR_WIDTH - 1;
+        addr_b : in  natural range 0 to 2**ADDR_WIDTH - 1;
+        data_a : in  std_logic_vector((DATA_WIDTH-1) downto 0);
+        data_b : in  std_logic_vector((DATA_WIDTH-1) downto 0);
+        we_a   : in  std_logic := '1';
+        we_b   : in  std_logic := '1';
+        q_a    : out std_logic_vector((DATA_WIDTH -1) downto 0);
+        q_b    : out std_logic_vector((DATA_WIDTH -1) downto 0)
+        );
+
+  end component;
+
+  type ram_type is array(0 to MEM_DEPTH-1) of std_logic_vector(7 downto 0);
+
 begin
 
-  -- PORT0
-  process(clk)
+  -----------------------------------------------------------------------------
+  -- I (JDV) had a whole bunch of trouble getting a proper byte enabled
+  -- doal ported ram. This solution (having a seperate entity) seems to work
+  -- in both modelsim and quartus
+  -----------------------------------------------------------------------------
+  byte_gen : for byte in byte_en0'range generate
+    signal wen0, wen1           : std_logic;
+    signal byte_in0, byte_in1   : std_logic_vector(7 downto 0);
+    signal byte_out0, byte_out1 : std_logic_vector(7 downto 0);
+
+    signal addr_a, addr_b : natural range 0 to MEM_DEPTH-1;
+    shared variable ram   : ram_type;
+
   begin
-    if rising_edge(clk) then
-      data_out0 <= ram(to_integer(unsigned(addr0)));
-      for byte in byte_en0'range loop
-        if (wr_en0 and byte_en0(byte)) = '1' then
-          ram(to_integer((unsigned(addr0))))((byte+1)*8 -1 downto byte*8) := data_in0((byte+1)*8 -1 downto byte*8);
-        end if;
-      end loop;  -- byte in byte_en0'range
-    end if;
-  end process;
+    wen0     <= wr_en0 and byte_en0(byte);
+    wen1     <= wr_en1 and byte_en0(byte);
+    byte_in0 <= data_in0((byte+1)*8-1 downto byte*8);
+    byte_in1 <= data_in1((byte+1)*8-1 downto byte*8);
 
-  -- PORT1
-  process(clk)
-  begin
-    if rising_edge(clk) then
-      data_out1 <= ram(to_integer(unsigned(addr1)));
-      for byte in byte_en1'range loop
-        if (wr_en1 and byte_en1(byte)) = '1' then
-          ram(to_integer((unsigned(addr1))))((byte+1)*8 -1 downto byte*8) := data_in1((byte+1)*8 -1 downto byte*8);
-        end if;
-      end loop;  -- byte in byte_en1'range
-    end if;
-  end process;
+    data_out0((byte+1)*8-1 downto byte*8) <= byte_out0;
+    data_out1((byte+1)*8-1 downto byte*8) <= byte_out1;
 
+    addr_a <= to_integer(unsigned(addr0));
+    addr_b <= to_integer(unsigned(addr1));
 
+    dpram : component true_dual_port_ram_single_clock
+      generic map (
+        DATA_WIDTH => 8,
+        ADDR_WIDTH => addr0'length)
+      port map (
+        clk    => clk,
+        data_a => byte_in0,
+        data_b => byte_in1,
+        addr_a => addr_a,
+        addr_b => addr_b,
+        we_a   => wen0,
+        we_b   => wen1,
+        q_a    => byte_out0,
+        q_b    => byte_out1);
+  end generate byte_gen;
 end architecture behav;
 
 
@@ -85,31 +184,31 @@ entity ram_4port is
     MEM_WIDTH : natural;
     FAMILY    : string := "ALTERA");
   port(
-    clk            : in  std_logic;
-    scratchpad_clk : in  std_logic;
-    reset          : in  std_logic;
-    stall_012      : out std_logic;
-    stall_3        : out std_logic;
+    clk            : in     std_logic;
+    scratchpad_clk : in     std_logic;
+    reset          : in     std_logic;
+    stall_012      : out    std_logic;
+    stall_3        : buffer std_logic;
                                         --read source A
-    raddr0         : in  std_logic_vector(log2(MEM_DEPTH)-1 downto 0);
-    ren0           : in  std_logic;
-    data_out0      : out std_logic_vector(MEM_WIDTH-1 downto 0);
+    raddr0         : in     std_logic_vector(log2(MEM_DEPTH)-1 downto 0);
+    ren0           : in     std_logic;
+    data_out0      : out    std_logic_vector(MEM_WIDTH-1 downto 0);
                                         --read source B
-    raddr1         : in  std_logic_vector(log2(MEM_DEPTH)-1 downto 0);
-    ren1           : in  std_logic;
-    data_out1      : out std_logic_vector(MEM_WIDTH-1 downto 0);
+    raddr1         : in     std_logic_vector(log2(MEM_DEPTH)-1 downto 0);
+    ren1           : in     std_logic;
+    data_out1      : out    std_logic_vector(MEM_WIDTH-1 downto 0);
                                         --write dest
-    waddr2         : in  std_logic_vector(log2(MEM_DEPTH)-1 downto 0);
-    byte_en2       : in  std_logic_vector(MEM_WIDTH/8-1 downto 0);
-    wen2           : in  std_logic;
-    data_in2       : in  std_logic_vector(MEM_WIDTH-1 downto 0);
+    waddr2         : in     std_logic_vector(log2(MEM_DEPTH)-1 downto 0);
+    byte_en2       : in     std_logic_vector(MEM_WIDTH/8-1 downto 0);
+    wen2           : in     std_logic;
+    data_in2       : in     std_logic_vector(MEM_WIDTH-1 downto 0);
                                         --external slave port
-    rwaddr3        : in  std_logic_vector(log2(MEM_DEPTH)-1 downto 0);
-    wen3           : in  std_logic;
-    ren3           : in  std_logic;     --cannot be asserted same cycle as wen3
-    byte_en3       : in  std_logic_vector(MEM_WIDTH/8-1 downto 0);
-    data_in3       : in  std_logic_vector(MEM_WIDTH-1 downto 0);
-    data_out3      : out std_logic_vector(MEM_WIDTH-1 downto 0));
+    rwaddr3        : in     std_logic_vector(log2(MEM_DEPTH)-1 downto 0);
+    wen3           : in     std_logic;
+    ren3           : in     std_logic;  --cannot be asserted same cycle as wen3
+    byte_en3       : in     std_logic_vector(MEM_WIDTH/8-1 downto 0);
+    data_in3       : in     std_logic_vector(MEM_WIDTH-1 downto 0);
+    data_out3      : out    std_logic_vector(MEM_WIDTH-1 downto 0));
 end entity;
 
 architecture rtl of ram_4port is
@@ -154,11 +253,13 @@ architecture rtl of ram_4port is
   signal cycle_count      : cycle_count_t;
   signal last_cycle_count : cycle_count_t;
 
-  signal sp_follow  : std_logic;
+  signal sp_follow : std_logic;
+
 begin  -- architecture rtl
 
   stall_012 <= '0';
-  stall_3   <= '0';
+  stall_3 <= '0';
+
 
   process(scratchpad_clk)
   begin
@@ -175,20 +276,20 @@ begin  -- architecture rtl
   cycle_count <= SLAVE_WRITE_CYCLE when sp_follow = '1' else READ_CYCLE;
 
   --Double clock select.
-  actual_byte_en0 <= byte_en2 when cycle_count = SLAVE_WRITE_CYCLE else (others => '-');
+  actual_byte_en0 <= byte_en2       when cycle_count = SLAVE_WRITE_CYCLE else (others => '-');
   actual_byte_en1 <= byte_en3 when cycle_count = SLAVE_WRITE_CYCLE else (others => '-');
 
-  actual_wr_en0 <= wen2 when cycle_count = SLAVE_WRITE_CYCLE else '0';
+  actual_wr_en0 <= wen2       when cycle_count = SLAVE_WRITE_CYCLE else '0';
   actual_wr_en1 <= wen3 when cycle_count = SLAVE_WRITE_CYCLE else '0';
 
-  actual_addr0 <= waddr2  when cycle_count = SLAVE_WRITE_CYCLE else raddr0;
+  actual_addr0 <= waddr2        when cycle_count = SLAVE_WRITE_CYCLE else raddr0;
   actual_addr1 <= rwaddr3 when cycle_count = SLAVE_WRITE_CYCLE else raddr1;
 
-  actual_data_in0 <= data_in2 when cycle_count = SLAVE_WRITE_CYCLE else (others => '-');
+  actual_data_in0 <= data_in2       when cycle_count = SLAVE_WRITE_CYCLE else (others => '-');
   actual_data_in1 <= data_in3 when cycle_count = SLAVE_WRITE_CYCLE else (others => '-');
 
   --save values for entire 1x clock
-  process(clk)
+  process(scratchpad_clk)
   begin
     if rising_edge(scratchpad_clk) then
       if cycle_count /= SLAVE_WRITE_CYCLE then
