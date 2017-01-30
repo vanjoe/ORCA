a32ccc846f67f69915c3abecee5a6ce97ce09d3b
Joel Vandergriendt <joel@vectorblox.com>
2017-01-30 13:39:05 -0800

    clean up memory initialization

--- ice40ultraplus/iCEcube2_flow.tcl
+++ ice40ultraplus/iCEcube2_flow.tcl
@@ -28,7 +28,7 @@ source $sbt_tcl
 
 #Parse command line arguments, currently if non-zero then run interactive
 if {$::argc == 4} {
-    set edif_file [lindex $::argv 0] 
+    set edif_file [lindex $::argv 0]
     set output_dir [lindex $::argv 1]
     set mem_file_name [lindex $::argv 2]
     set list_file_name [lindex $::argv 3]
@@ -59,6 +59,10 @@ if {$::argc == 4} {
 		#		set ram_file_name [lindex $ram_file_names $i]
 				if {[file exists $ram_file_name]} {
 					 file delete $ram_file_name
+				}
+				set dir_name [file dirname $ram_file_name]
+				if { ![file exists $dir_name]} {
+					 file mkdir $dir_name
 				}
 				set ram_file [open $ram_file_name "w"]
 				lappend ram_files $ram_file
@@ -96,13 +100,13 @@ if {$::argc == 4} {
 
     }
 } elseif {$::argc == 3} {
-    set edif_file [lindex $::argv 0] 
+    set edif_file [lindex $::argv 0]
     set output_dir [lindex $::argv 1]
     puts "Three arguments passed, regenerating bits."
     sbt_init_env
     sbt_run_bitmap $top_module $device $output_dir $tool_options
 } elseif {$::argc == 2} {
-    set edif_file [lindex $::argv 0] 
+    set edif_file [lindex $::argv 0]
     set output_dir [lindex $::argv 1]
     puts "Two arguments passed, running full backend."
     run_sbt_backend_auto $device $top_module $proj_dir $output_dir $tool_options $edif_file
