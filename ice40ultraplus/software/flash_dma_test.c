1a1e23f56e9756c053a7ced1f41362cd4aff3ad2
Aaron Severance <aseverance@vectorblox.com>
2017-02-15 12:49:53 -0800

    ice40ultraplus flash_dma_test now more thorough, uses golden.bin.

--- ice40ultraplus/software/flash_dma_test.c
+++ ice40ultraplus/software/flash_dma_test.c
@@ -37,17 +37,33 @@ int do_lve(void* base){
 }
 
 #define DO_SPRAM_TEST 1
+#define TEST_RUNS     255
+
+extern const int      golden_size;
+extern const uint16_t golden_BSD_checksums[];
+
+uint16_t bsd_checksum(char *data, int bytes){
+	int byte;
+	uint16_t checksum = 0;
+	//From https://en.wikipedia.org/wiki/BSD_checksum
+	for(byte = 0; byte < bytes; byte++){
+		checksum = (checksum >> 1) + ((checksum & 1) << 15);
+		checksum += data[byte];
+		checksum &= 0xffff;
+	}
+	return checksum;
+}
 
 int main()
 {
 
 	printf("TEST!\r\n");
 
-	int xfer_size=64*1024;
-	volatile char* sp_base=(volatile char*)SCRATCHPAD_BASE;
+	volatile char* sp_base = (volatile char*)SCRATCHPAD_BASE;
 	int i;
-	int spram_errors=0;
+	int spram_errors = 0;
 	printf("\r\n");
+
 #if DO_SPRAM_TEST
 	//SPRAM TEST:
 	int spram_test_len=1024;
@@ -63,43 +79,62 @@ int main()
 	}
 #endif
 	//wait while initializing
-	while(FLASH_DMA_BASE[FLASH_DMA_STATUS] & 0x80000000 ){
+	while(FLASH_DMA_BASE[FLASH_DMA_STATUS] & 0x80000000){
 		printf("waiting for Flash initialization\r\n");
 	}
 
-
-	for(i=0;i<xfer_size;i++){
-		sp_base[i]=0xFF;
-	}
-
-	int start_time=get_time();
 	int flash_address=0*1024;
 
+	int checksum_errors=0;
 	int lve_errors=0;
-	int dma_errors;
-	do{
-		flash_dma_trans(flash_address,(void*)sp_base,xfer_size);
-		lve_errors=do_lve(SCRATCHPAD_BASE+xfer_size);
-		//wait for transfer done
-		while(!flash_dma_done());
-
-		//since the LVE does some printing that can take longer than the
-		//dma transfer, the cycle count might not be strictly correct.
-		//it should be about 19 cycles per byte, + interference
-		printf("%d bytes read in %d cycles\r\n",xfer_size,get_time()-start_time);
-		dma_errors=0;
-		for(i=0;i<xfer_size/4;i++){
-			int a=i, b=((int*)sp_base)[i];
-			if (a!=b && a !=0x601 ){
-				dma_errors++;
-				if(dma_errors <10){
-					//only print the first 10 errors
-					printf("%d %x\r\n",a,b);
-				}
+	int run;
+	for(run = 0; run < TEST_RUNS; run++){
+		printf("Run %d\r\n", run);
+		int xfer_size=64*1024;
+		int chunk = 0;
+		for(flash_address = 0; flash_address < golden_size; flash_address += xfer_size){
+			if(flash_address + xfer_size > golden_size){
+				xfer_size = golden_size - flash_address;
 			}
+			for(i=0;i<xfer_size;i++){
+				sp_base[i]=0xFF;
+			}
+			printf("Chunk %d: Transferring %d bytes starting at %d\r\n", chunk, xfer_size, flash_address);
+			
+			int start_time = get_time();
+			flash_dma_trans(flash_address, (void*)sp_base, xfer_size);
+			int local_lve_errors = do_lve(SCRATCHPAD_BASE + xfer_size);
+			//wait for transfer done
+			while(!flash_dma_done());
+			int end_time = get_time();
+
+			//since the LVE does some printing that can take longer than the
+			//dma transfer, the cycle count might not be strictly correct.
+			//it should be about 19 cycles per byte, + interference
+			printf("%d bytes read in %d cycles (%d lve errors)\r\n", xfer_size, end_time-start_time, local_lve_errors);
+
+			uint16_t chunk_BSD_checksum = bsd_checksum((char *)sp_base, xfer_size);
+			if(chunk_BSD_checksum != golden_BSD_checksums[chunk]){
+				checksum_errors++;
+				printf("Checksum error: expected %u got %u\r\n", golden_BSD_checksums[chunk], chunk_BSD_checksum);
+				printf("First 16 bytes read:");
+				int byte = 0;
+				for(byte = 0; byte < 16; byte++){
+					if(!(byte & 0x1)){
+						printf(" ");
+					}
+					printf("%02X", sp_base[byte]);
+				}
+				printf("\r\n");
+			}
+
+			printf("\r\n");
+			lve_errors += local_lve_errors;
+			chunk++;
 		}
 
-	}while(0);
+		printf("Running total errors: LVE %d DMA %d\r\n\r\n\r\n", lve_errors, checksum_errors);
+	}
 
 	printf("DONE!!\r\n");
 
@@ -107,9 +142,9 @@ int main()
 		printf("SPRAM ERRORS :(\r\n");
 	}if(lve_errors){
 		printf("LVE ERRORS :(\r\n");
-	}if(dma_errors){
-		printf("DMA ERRORS :( (Assuming you initialized the flash properly)\r\n");
-	}if(spram_errors + lve_errors + dma_errors == 0){
+	}if(checksum_errors){
+		printf("DMA ERRORS :( (Assuming you initialized the flash properly with golden.bin)\r\n");
+	}if(spram_errors + lve_errors + checksum_errors == 0){
 		printf ("No Errors :)\r\n");
 	}
 
---
+++ ice40ultraplus/software/golden.c
@@ -0,0 +1,13 @@
+#include <stdint.h>
+const int      golden_size = 566032;
+const uint16_t golden_BSD_checksums[] = {
+50011,
+34323,
+26824,
+31692,
+26499,
+19585,
+31622,
+11252,
+51360,
+};
