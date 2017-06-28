4951b4e0538b127f8e1eee3a11a2d1168f2d5ab6
ryan <ryan@vectorblox.com>
2017-06-28 14:30:44 -0700

    All bad file handles now get removed from the sf2plus project.

--- systems/sf2plus/helpers.py
+++ systems/sf2plus/helpers.py
@@ -40,38 +40,6 @@ def fix_cxf(cxf_files):
 		file_to_write = open(cxf, 'w')
 		file_to_write.write(cxf_string)
 		file_to_write.close()
-		#for line in cxf_string.split('\n'):
-		#	file_to_write.write(line)
-		#file_to_write.close()
-		
-
-		#cxf_tree.write(cxf, encoding='UTF-8', xml_declaration=True, method='xml')
-			
-
-
-		# Remove any whitespace to allow Libero to parse the .cxf files.
-		# This may seem more verbose than neccessary, but this was the order required to remove
-		# each character correctly.
-		#cxf_dom = xml.dom.minidom.parseString(xml.etree.ElementTree.tostring(root, encoding = 'UTF-8', method='xml'))
-		#file_to_write = open(cxf, 'w')
-		#file_to_write.write(cxf_dom.toprettyxml(indent='\t'))
-		#file_to_write.close()
-		
-		# Remove the \t characters.
-		#subprocess.Popen('sed -i -e \'s/\\t//g\' {}'.format(cxf), shell=True).wait()
-		# Add the xml header.	
-		#subprocess.Popen('sed -i -e \'s/<[?][^?]\+[?]>/<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\" ?>/g\' {}'.format(cxf), shell=True).wait()
-		
-			
-		# Remove the \n characters.
-		#file_to_read = open(cxf, 'r')
-		#file_to_read_text = file_to_read.read()
-		#file_to_read.close()
-		#file_to_write = open(cxf, 'w')
-		#for line in file_to_read_text.split('\n'):
-		#	file_to_write.write(line)
-		#file_to_write.close()
-
 
 # This helper cleans up bad file handles from the project file.
 def fix_prj(prj_file):
@@ -83,26 +51,32 @@ def fix_prj(prj_file):
 
 	file_manager = False
 	value = False
-	bad_handle = False
-	value_string = ''
+	bad_file = False
 	for line in lines:
+		# Set bad_handle to be true if a non-FILE value contains a bad file handle. The bad 
+		# handle will be removed.
+		bad_handle = False
+		if 'VALUE' in line:
+			if 'nfs' in line: 
+				bad_handle = True
+		# Check if we're in the FileManager section. VALUE's in the FileManager section cover
+		# multiple lines, so multiple lines need to be ignored.
 		if 'FileManager' in line:
 			file_manager = True
 		elif file_manager:
 			if 'VALUE' in line:
 				value = True
-				file_handle = re.findall(r'<project>[^,]+')
-				if not file_handle:
-					bad_handle = True	
-
-		if not bad_handle:
+			if bad_handle:
+					bad_file = True	
+	
+		if not (bad_file or bad_handle):
 			file_to_write.write(line + '\n')
 
 		if file_manager and ('ENDLIST' in line):
 			file_manager = False
+			bad_file = False
 		elif value and ('ENDFILE' in line):
 			value = False
-			bad_handle = False
-			value_string = ''
+			bad_file = False
 			
 	file_to_write.close()
