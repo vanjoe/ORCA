70cfa17a75a60aa76c2af5d06a669c8bbdf9b0eb
Joel Vandergriendt <joel@vectorblox.com>
2017-07-24 16:59:49 -0700

    build toolchain with upsteam sources

--- tools/riscv-toolchain/build-toolchain.sh
+++ tools/riscv-toolchain/build-toolchain.sh
@@ -4,45 +4,80 @@ then
         echo "RISCV_INSTALL not defined, please define it to path for installing toolchain ... exiting" >&2
         exit 1
 fi
+if [ -z "$BUILD_DIR" ]
+then
+	 BUILD_DIR=./build
+fi
+SCRIPT_DIR=$(readlink -f $(dirname $0))
 
-#get relevant git repositories, at the correct commit
-git clone https://github.com/riscv/riscv-gnu-toolchain
-(cd riscv-gnu-toolchain;
- git checkout -q ff21e26eb8c4d55dad7ad0b57e7bd8f7784a60e9;
- git submodule update --init --recursive
- )
+mkdir -p $BUILD_DIR
+cd $BUILD_DIR
 
-if [ -f opcodes-lve.py ]
+BINUTILS_VERSION=2.28
+GCC_VERSION=7.1.0
+
+[ ! -f binutils-$BINUTILS_VERSION.tar.gz ] && wget http://ftpmirror.gnu.org/binutils/binutils-$BINUTILS_VERSION.tar.gz
+[ ! -f gcc-$GCC_VERSION.tar.gz ] && wget http://ftpmirror.gnu.org/gcc/gcc-$GCC_VERSION/gcc-$GCC_VERSION.tar.gz
+(
+	 rm -rf binutils-$BINUTILS_VERSION gcc-$GCC_VERSION
+	 tar -xf binutils-$BINUTILS_VERSION.tar.gz &
+	 tar -xf gcc-$GCC_VERSION.tar.gz &
+	 wait
+)
+
+export PATH=$RISCV_INSTALL/bin:$PATH
+
+## ADD LVE instructions to the assembler.
+OPCODES_PY_FILE=$SCRIPT_DIR/opcodes-lve.py
+if [ -f $OPCODES_PY_FILE ]
 then
 	 git clone https://github.com/riscv/riscv-opcodes.git
 	 (cd riscv-opcodes;
-	  git checkout -q 3c1a9110b71658f6e3249186e8b44e7474a4ee90;
+	  git checkout -q e0abc2255a71afb0236032ae3d92bea26c15716d
 	 )
+	 python $OPCODES_PY_FILE > opcodes-lve
 
-	 #edit the opcodes to include lve instructions
-	 python opcodes-lve.py > opcodes-lve
-	 python opcodes-lve.py --riscv-opc > lve_extensions.h
+	 RISCV_OPC_H=binutils-$BINUTILS_VERSION/include/opcode/riscv-opc.h
+	 cat opcodes-lve | python $SCRIPT_DIR/riscv-opcodes/parse-opcodes -c | grep "#define \(MATCH_V\|MASK_V\)" > riscv-lve.h
+	 mv riscv-lve.h  $(dirname $RISCV_OPC_H)
+	 sed -i '/#define RISCV_ENCODING_H/a #include "riscv-lve.h"'  $RISCV_OPC_H
 
-	 OPCODE_FILES="opcodes-lve \
-	 riscv-opcodes/opcodes-pseudo \
-	 riscv-opcodes/opcodes \
-	 riscv-opcodes/opcodes-rvc \
-	 riscv-opcodes/opcodes-rvc-pseudo"
-
-	 RISCV_OPC_H=riscv-gnu-toolchain/riscv-binutils-gdb/include/opcode/riscv-opc.h
-	 cat $OPCODE_FILES | python riscv-opcodes/parse-opcodes -c > $RISCV_OPC_H
-
-
-	 RISCV_OPC_C=riscv-gnu-toolchain/riscv-binutils-gdb/opcodes/riscv-opc.c
+	 python $OPCODES_PY_FILE --riscv-opc > lve_extensions.h
+	 RISCV_OPC_C=binutils-$BINUTILS_VERSION/opcodes/riscv-opc.c
 	 mv lve_extensions.h $(dirname $RISCV_OPC_C)
 	 sed -i 's/#include "lve_extensions.h"//' $RISCV_OPC_C
 	 sed -i  '/\ Terminate the list.  /i#include "lve_extensions.h"' $RISCV_OPC_C
+
 fi
 
-# start the compilation/build
-cd riscv-gnu-toolchain
-mkdir build
-cd build
-../configure --prefix=$RISCV_INSTALL --with-arch=rv32im --with-abi=ilp32
+#binutils
+(
 
-make -j $(( $(nproc)*2 ))
+	 mkdir build-binutils
+	 cd build-binutils
+	 ../binutils-$BINUTILS_VERSION/configure --prefix=$RISCV_INSTALL --with-abi=ilp32 --target=riscv32-unknown-elf --disable-multilib
+	 make -j$(( `nproc` * 2))
+	 make install
+)
+
+#gcc
+(
+	 mkdir -p build-gcc
+	 cd build-gcc
+	 ../gcc-$GCC_VERSION/configure --prefix=$RISCV_INSTALL --target=riscv32-unknown-elf --with-abi=ilp32 --enable-languages=c,c++ --disable-multilib
+	 make -j$(( `nproc` * 2))  all-gcc
+	 make install-gcc
+	 make -j$(( `nproc` * 2))  all-target-libgcc
+	 make install-target-libgcc
+
+
+)
+#newlib (still uses riscv git repository not yet upstreamed
+(
+	 git clone -b riscv-newlib-2.5.0 https://github.com/riscv/riscv-newlib.git
+	 mkdir build-newlib
+	 cd build-newlib
+	 ../riscv-newlib/configure --target=riscv32-unknown-elf --prefix=$RISCV_INSTALL
+	 make
+	 make install
+)
