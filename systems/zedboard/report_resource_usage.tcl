9cb90cc0c6432a667276c284d84661fd1f0f1321
Aaron Severance <aseverance@vectorblox.com>
2017-09-08 14:55:11 -0700

    Updated build scripts for Orca.

--- systems/zedboard/report_resource_usage.tcl
+++ systems/zedboard/report_resource_usage.tcl
@@ -1,5 +1,5 @@
-proc report_resources {proj_dir proj_name} {
+proc report_resources {proj_dir proj_name output_file} {
 	open_project $proj_dir/$proj_name.xpr
 	open_run impl_1
-	report_utilization -hierarchical -file resource_utilization.rpt
+	report_utilization -hierarchical -file $output_file
 }
