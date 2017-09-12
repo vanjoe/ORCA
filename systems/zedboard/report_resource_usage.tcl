9769df36cfd1c24d574b043d5540d3e977f54eca
Aaron Severance <aseverance@vectorblox.com>
2017-09-12 14:46:49 -0700

    Have Orca Xilinx tests running again.

--- systems/zedboard/report_resource_usage.tcl
+++ systems/zedboard/report_resource_usage.tcl
@@ -3,3 +3,10 @@ proc report_resources {proj_dir proj_name output_file} {
 	open_run impl_1
 	report_utilization -hierarchical -file $output_file
 }
+
+if {$argc > 0} {
+    regsub -all {\\\{} $argv "{" argv
+    regsub -all {\\\}} $argv "}" argv
+    puts "Executing [set argv]"
+    eval [set argv]
+}
