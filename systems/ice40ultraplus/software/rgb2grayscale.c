e4b579cb704230d55f185e1339dd7e226aae7322
nivanov94 <nicolas.ivanov94@gmail.com>
2017-11-16 11:37:55 -0800

    Added grayscale camera option for image difference filter.

--- systems/ice40ultraplus/software/rgb2grayscale.c
+++ systems/ice40ultraplus/software/rgb2grayscale.c
@@ -9,34 +9,34 @@
  */
 void convert_rgb2grayscale(vbx_word_t* rgb,vbx_word_t* gs)
 {
-	vbx_word_t* tmp = (vbx_word_t*)(gs + 4*THUMB_WIDTH*THUMB_HEIGHT);
+	vbx_word_t* tmp = (vbx_word_t*)(gs + 4*THUMBNAIL_WIDTH*THUMBNAIL_HEIGHT);
 
-	vbx_set_vl(THUMB_WIDTH);
-	for(int row=0;row<THUMB_HEIGHT;row++){
+	vbx_set_vl(THUMBNAIL_WIDTH);
+	for(int row=0;row<THUMBNAIL_HEIGHT;row++){
 	
 		// extract B pixel
 		vbx(SVW,VAND,tmp,0xFF,rgb + row*CAM_IMG_WIDTH);
 		// move the weighted B into the grayscale image
-		vbx(SVW,VMUL,gs + row*THUMB_WIDTH,25,tmp);
+		vbx(SVW,VMUL,gs + row*THUMBNAIL_WIDTH,25,tmp);
 	
 		// extract G pixel
 		vbx(SVW,VAND,tmp,0xFF00,rgb + row*CAM_IMG_WIDTH);
 		vbx(SVW,VMULH,tmp,(1<<24),tmp);
 		// add the weighted G pixel into the grayscale image
 		vbx(SVW,VMUL,tmp,129,tmp);
-		vbx(VVW,VADD,gs + row*THUMB_WIDTH,gs + row*THUMB_WIDTH,tmp);
+		vbx(VVW,VADD,gs + row*THUMBNAIL_WIDTH,gs + row*THUMBNAIL_WIDTH,tmp);
 	
 		//extract the R pixel
 		vbx(SVW,VAND,tmp,0xFF0000,rgb + row*CAM_IMG_WIDTH);
 		vbx(SVW,VMULH,tmp,(1<<16),tmp);
 		// add the weighted R pixel into the gayscale image
 		vbx(SVW,VMUL,tmp,66,tmp);
-		vbx(VVW,VADD,gs + row*THUMB_WIDTH,gs + row*THUMB_WIDTH,tmp);
+		vbx(VVW,VADD,gs + row*THUMBNAIL_WIDTH,gs + row*THUMBNAIL_WIDTH,tmp);
 	
 		// convert to 8 bit grayscale pixel
-		vbx(SVW,VADD,gs + row*THUMB_WIDTH,128,gs + row*THUMB_WIDTH); // for rounding
-		vbx(SVW,VAND,gs + row*THUMB_WIDTH,0xFFFF,gs + row*THUMB_WIDTH);
-		vbx(SVW,VMULH,gs + row*THUMB_WIDTH,(1<<24),gs + row*THUMB_WIDTH);
+		vbx(SVW,VADD,gs + row*THUMBNAIL_WIDTH,128,gs + row*THUMBNAIL_WIDTH); // for rounding
+		vbx(SVW,VAND,gs + row*THUMBNAIL_WIDTH,0xFFFF,gs + row*THUMBNAIL_WIDTH);
+		vbx(SVW,VMULH,gs + row*THUMBNAIL_WIDTH,(1<<24),gs + row*THUMBNAIL_WIDTH);
 	}
 }
 
